28b4d1050dd1892dc123f13fdea42ce6e9f699f3
NewUserName <danielpejcinovski@hotmail.com>
2017-02-21 12:58:17 +0100

    Made fix to dependency.

--- src/Vulkan/VulkanRenderer.cpp
+++ src/Vulkan/VulkanRenderer.cpp
@@ -529,9 +529,11 @@ void VulkanRenderer::createRenderPass(VkFormat format) {
 
     VkSubpassDependency dependency = {};
     dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
-    dependency.dstSubpass = 0;
     dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
-    dependency.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+    dependency.srcAccessMask  = 0;
+    dependency.dstSubpass = 0;
+    dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+    dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
 
     // We use a single color buffer.
     VkAttachmentDescription colorAttachment = {};
