bfebd1770432bf696f54c93ea9dc46f98edecfc6
Albin Bernhardsson <catscratchesp@gmail.com>
2017-03-06 14:21:27 +0100

    Create staging image

--- src/Vulkan/Texture2DVulkan.cpp
+++ src/Vulkan/Texture2DVulkan.cpp
@@ -1,15 +1,14 @@
 #include "Texture2DVulkan.hpp"
 
 #include <iostream>
-#include <vulkan/vulkan.h>
 #include <stb_image.h>
 
 #define UNIMPLEMENTED {\
 std::cout << "Unimplemented method in: " << __FILE__ << ":" << __LINE__ << std::endl;\
 }
 
-Texture2DVulkan::Texture2DVulkan() {
-    
+Texture2DVulkan::Texture2DVulkan(VkDevice device) {
+    this->device = device;
 }
 
 Texture2DVulkan::~Texture2DVulkan() {
@@ -23,6 +22,28 @@ int Texture2DVulkan::loadFromFile(std::string filename) {
     VkDeviceSize imageSize = width * height * 4;
     if (pixels == nullptr) {
         std::cerr << "Failed to load image " << filename << std::endl;
+        exit(-1);
+    }
+    
+    // Create staging image.
+    VkImageCreateInfo imageInfo = {};
+    imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+    imageInfo.imageType = VK_IMAGE_TYPE_2D;
+    imageInfo.extent.width = width;
+    imageInfo.extent.height = height;
+    imageInfo.extent.depth = 1;
+    imageInfo.mipLevels = 1;
+    imageInfo.arrayLayers = 1;
+    imageInfo.format = VK_FORMAT_R8G8B8A8_UNORM;
+    imageInfo.tiling = VK_IMAGE_TILING_LINEAR;
+    imageInfo.initialLayout = VK_IMAGE_LAYOUT_PREINITIALIZED;
+    imageInfo.usage = VK_IMAGE_USAGE_TRANSFER_SRC_BIT;
+    imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+    imageInfo.flags = 0;
+    
+    if (vkCreateImage(device, &imageInfo, nullptr, &stagingImage) != VK_SUCCESS) {
+        std::cerr << "Failed to create image." << std::endl;
         exit(-1);
     }
     
--- src/Vulkan/Texture2DVulkan.hpp
+++ src/Vulkan/Texture2DVulkan.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
 #include "../Base/Texture2D.hpp"
+#include <vulkan/vulkan.h>
 
 class Texture2DVulkan : public Texture2D {
     public:
-        Texture2DVulkan();
+        Texture2DVulkan(VkDevice device);
         ~Texture2DVulkan() final;
         
         int loadFromFile(std::string filename) final;
         void bind(unsigned int slot) final;
+        
+    private:
+        VkDevice device;
+        VkImage stagingImage;
+        VkDeviceMemory stagingImageMemory;
 };
