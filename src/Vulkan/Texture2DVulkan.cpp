0fc1b6669197cf6cb408acff4fc4b50b6d0ecae1
Albin Bernhardsson <catscratchesp@gmail.com>
2017-03-06 14:03:18 +0100

    Load image data from file

--- src/Vulkan/Texture2DVulkan.cpp
+++ src/Vulkan/Texture2DVulkan.cpp
@@ -1,6 +1,8 @@
 #include "Texture2DVulkan.hpp"
 
 #include <iostream>
+#include <vulkan/vulkan.h>
+#include <stb_image.h>
 
 #define UNIMPLEMENTED {\
 std::cout << "Unimplemented method in: " << __FILE__ << ":" << __LINE__ << std::endl;\
@@ -15,7 +17,20 @@ Texture2DVulkan::~Texture2DVulkan() {
 }
 
 int Texture2DVulkan::loadFromFile(std::string filename) {
+    // Load texture from file.
+    int width, height, channels;
+    stbi_uc* pixels = stbi_load(filename.c_str(), &width, &height, &channels, STBI_rgb_alpha);
+    VkDeviceSize imageSize = width * height * 4;
+    if (pixels == nullptr) {
+        std::cerr << "Failed to load image " << filename << std::endl;
+        exit(-1);
+    }
+    
     UNIMPLEMENTED
+    
+    // Clean up.
+    delete[] pixels;
+    
     return 0;
 }
 
