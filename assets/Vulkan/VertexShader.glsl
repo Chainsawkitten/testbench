989e617768749feb2b9fbc14e2ee972b203d41b6
NewUserName <danielpejcinovski@hotmail.com>
2017-03-31 15:21:55 +0200

    Made fixes according to comments.

--- assets/Vulkan/VertexShader.glsl
+++ assets/Vulkan/VertexShader.glsl
@@ -1,5 +1,5 @@
 // buffer inputs
-/*#ifdef NORMAL
+#ifdef NORMAL
 	layout(binding=NORMAL) buffer nor { vec4 normal_in[]; };
 	layout(location=NORMAL) out vec4 normal_out;
 #endif
@@ -8,37 +8,20 @@
 	layout(binding=TEXTCOORD) buffer text { vec2 uv_in[]; };
 	layout(location=TEXTCOORD) out vec2 uv_out;
 #endif
-layout(binding=POSITION) buffer pos { vec4 position_in[]; };*/
+layout(binding=POSITION) buffer pos { vec4 position_in[]; };
 
-// uniform block
-// layout(std140, binding = 20) uniform TransformBlock
-// {
-//  	vec4 tx;
-// } transform;
-
-layout(binding=TRANSLATION) buffer TRANSLATION_NAME {
+layout(binding=TRANSLATION) uniform TRANSLATION_NAME
+{
 	vec4 translate;
 };
 
-layout(binding=POSITION) buffer Position {
-	vec4 position;
+layout(binding=DIFFUSE_TINT) uniform DIFFUSE_TINT_NAME
+{
+	vec4 diffuseTint;
 };
-
-#ifdef NORMAL
-layout(binding=NORMAL) buffer Normal {
-	vec4 normal;
-};
-layout(location=NORMAL) out vec4 normal_out;
-#endif TEXTCOORD
-
-#ifdef TEXTCOORD
-	layout(binding=TEXTCOORD) buffer Textcoord {
-		vec2 UV;
-	};
-	layout(location=TEXTCOORD) out vec2 uv_out;
-#endif
 
 void main() {
+
 	#ifdef NORMAL
 		normal_out = normal_in[gl_VertexIndex];
 	#endif
@@ -47,8 +30,5 @@ void main() {
 		uv_out = uv_in[gl_VertexIndex];
 	#endif
 
-	//gl_Position = position_in[gl_VertexIndex] + translate;
-	
-    
-    gl_Position = position + translate;
+	gl_Position = position_in[gl_VertexIndex] + translate;
 }
