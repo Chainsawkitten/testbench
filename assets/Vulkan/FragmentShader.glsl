98b1b74f95531d9c9e051f70e22e08f5486d4daa
NewUserName <danielpejcinovski@hotmail.com>
2017-03-31 15:28:17 +0200

    Converted tabs to spaces.

--- assets/Vulkan/FragmentShader.glsl
+++ assets/Vulkan/FragmentShader.glsl
@@ -1,17 +1,17 @@
 // inputs
 #ifdef NORMAL
-	layout( location = NORMAL ) in vec4 normal_in;
+    layout( location = NORMAL ) in vec4 normal_in;
 #endif
 
 #ifdef TEXTCOORD
-	layout (location = TEXTCOORD ) in vec2 uv_in;
+    layout (location = TEXTCOORD ) in vec2 uv_in;
 #endif
 
 layout(location = 0) out vec4 fragment_color;
 
 layout(binding=DIFFUSE_TINT) uniform DIFFUSE_TINT_NAME
 {
-	vec4 diffuseTint;
+    vec4 diffuseTint;
 };
 
 // binding sets the TEXTURE_UNIT value!
@@ -20,11 +20,11 @@ layout(binding=DIFFUSE_SLOT) uniform sampler2D myTex;
 #endif
 
 void main () {
-	#ifdef DIFFUSE_SLOT
-		vec4 col = texture(myTex, uv_in);
-	#else
-		vec4 col = vec4(1.0,1.0,0.0, 1.0);
-	#endif
+    #ifdef DIFFUSE_SLOT
+        vec4 col = texture(myTex, uv_in);
+    #else
+        vec4 col = vec4(1.0,1.0,0.0, 1.0);
+    #endif
 
-	fragment_color = col * vec4(diffuseTint.rgb,1.0);
+    fragment_color = col * vec4(diffuseTint.rgb,1.0);
 }
--- assets/Vulkan/VertexShader.glsl
+++ assets/Vulkan/VertexShader.glsl
@@ -1,34 +1,34 @@
 // buffer inputs
 #ifdef NORMAL
-	layout(binding=NORMAL) buffer nor { vec4 normal_in[]; };
-	layout(location=NORMAL) out vec4 normal_out;
+    layout(binding=NORMAL) buffer nor { vec4 normal_in[]; };
+    layout(location=NORMAL) out vec4 normal_out;
 #endif
 
 #ifdef TEXTCOORD
-	layout(binding=TEXTCOORD) buffer text { vec2 uv_in[]; };
-	layout(location=TEXTCOORD) out vec2 uv_out;
+    layout(binding=TEXTCOORD) buffer text { vec2 uv_in[]; };
+    layout(location=TEXTCOORD) out vec2 uv_out;
 #endif
 layout(binding=POSITION) buffer pos { vec4 position_in[]; };
 
 layout(binding=TRANSLATION) uniform TRANSLATION_NAME
 {
-	vec4 translate;
+    vec4 translate;
 };
 
 layout(binding=DIFFUSE_TINT) uniform DIFFUSE_TINT_NAME
 {
-	vec4 diffuseTint;
+    vec4 diffuseTint;
 };
 
 void main() {
 
-	#ifdef NORMAL
-		normal_out = normal_in[gl_VertexIndex];
-	#endif
+    #ifdef NORMAL
+        normal_out = normal_in[gl_VertexIndex];
+    #endif
 
-	#ifdef TEXTCOORD
-		uv_out = uv_in[gl_VertexIndex];
-	#endif
+    #ifdef TEXTCOORD
+        uv_out = uv_in[gl_VertexIndex];
+    #endif
 
-	gl_Position = position_in[gl_VertexIndex] + translate;
+    gl_Position = position_in[gl_VertexIndex] + translate;
 }
