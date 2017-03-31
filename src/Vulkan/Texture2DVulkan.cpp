ed83a8f21ba748af3b9bb8a24a2626cd3e49dbcf
Albin Bernhardsson <catscratchesp@gmail.com>
2017-03-31 12:30:40 +0200

    Fix image creation
    Use format, tiling and usage information when creating image.

--- src/Vulkan/Texture2DVulkan.cpp
+++ src/Vulkan/Texture2DVulkan.cpp
@@ -87,10 +87,10 @@ void Texture2DVulkan::createImage(uint32_t width, uint32_t height, VkFormat form
     imageInfo.extent.depth = 1;
     imageInfo.mipLevels = 1;
     imageInfo.arrayLayers = 1;
-    imageInfo.format = VK_FORMAT_R8G8B8A8_UNORM;
-    imageInfo.tiling = VK_IMAGE_TILING_LINEAR;
+    imageInfo.format = format;
+    imageInfo.tiling = tiling;
     imageInfo.initialLayout = VK_IMAGE_LAYOUT_PREINITIALIZED;
-    imageInfo.usage = VK_IMAGE_USAGE_TRANSFER_SRC_BIT;
+    imageInfo.usage = usage;
     imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
     imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
     imageInfo.flags = 0;
