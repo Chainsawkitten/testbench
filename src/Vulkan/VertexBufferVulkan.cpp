4426043c5364ccce87b9f171f427ef2e2bd1f71f
Albin Bernhardsson <catscratchesp@gmail.com>
2017-04-04 14:10:09 +0200

    Indicate that vertex buffer will be used as such

--- src/Vulkan/VertexBufferVulkan.cpp
+++ src/Vulkan/VertexBufferVulkan.cpp
@@ -54,7 +54,7 @@ void VertexBufferVulkan::bind(size_t offset, size_t size, unsigned int location)
         offsetMap[location] = 0;
         
         // Create storage buffer.
-        createBuffer(size * 2000, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, &bufferMap[location], &memoryMap[location]);
+        createBuffer(size * 2000, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, &bufferMap[location], &memoryMap[location]);
     } else
         offsetMap[location]++;
     
