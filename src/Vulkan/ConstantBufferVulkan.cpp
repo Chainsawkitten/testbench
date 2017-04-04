92da4dfd38fcb335df7ab6e5d0428f08d9b45e58
Albin Bernhardsson <catscratchesp@gmail.com>
2017-04-04 14:10:49 +0200

    Indicate that uniform buffer will be used as such

--- src/Vulkan/ConstantBufferVulkan.cpp
+++ src/Vulkan/ConstantBufferVulkan.cpp
@@ -31,7 +31,7 @@ void ConstantBufferVulkan::setData(const void* data, size_t size, Material* m, u
         offsetMap[location] = 0;
         
         // Create uniform buffer.
-        VkDeviceSize deviceSize = createBuffer(size * 2000, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, &bufferMap[location], &memoryMap[location]);
+        VkDeviceSize deviceSize = createBuffer(size * 2000, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, &bufferMap[location], &memoryMap[location]);
         
         // Create descriptor set layout.
         createDescriptorLayout();
