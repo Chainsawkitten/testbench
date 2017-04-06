78b4405066be553e69ff846a3295cfb9576eff13
Albin Bernhardsson <catscratchesp@gmail.com>
2017-04-06 12:31:32 +0200

    Specify descriptor set layout in pipeline layout

--- src/Vulkan/MaterialVulkan.cpp
+++ src/Vulkan/MaterialVulkan.cpp
@@ -134,6 +134,8 @@ int MaterialVulkan::compileMaterial(std::string& errString) {
     // Pipeline layout.
     VkPipelineLayoutCreateInfo pipelineLayoutInfo = {};
     pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutInfo.setLayoutCount = 1;
+    pipelineLayoutInfo.pSetLayouts = &descriptorSetLayout;
     
     if (vkCreatePipelineLayout(device, &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
         std::cerr << "Failed to create pipeline layout." << std::endl;
