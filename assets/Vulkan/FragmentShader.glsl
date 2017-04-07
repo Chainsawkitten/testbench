059d917048e89b6386eae06855bdede082c68b15
Albin Bernhardsson <catscratchesp@gmail.com>
2017-04-07 12:40:02 +0200

    Create texture descriptor layout in material

--- assets/Vulkan/FragmentShader.glsl
+++ assets/Vulkan/FragmentShader.glsl
@@ -15,17 +15,16 @@ layout(set=4, binding=DIFFUSE_TINT) uniform DIFFUSE_TINT_NAME
 };
 
 // binding sets the TEXTURE_UNIT value!
-/*#ifdef DIFFUSE_SLOT
-layout(binding=DIFFUSE_SLOT) uniform sampler2D myTex;
-#endif*/
+#ifdef DIFFUSE_SLOT
+layout(set=5, binding=DIFFUSE_SLOT) uniform sampler2D myTex;
+#endif
 
 void main () {
-    /*#ifdef DIFFUSE_SLOT
+    #ifdef DIFFUSE_SLOT
         vec4 col = texture(myTex, uv_in);
     #else
-        vec4 col = vec4(1.0,1.0,0.0, 1.0);
-    #endif*/
+        vec4 col = vec4(1.0, 1.0, 0.0, 1.0);
+    #endif
     
-    vec4 col = vec4(1.0, 1.0, 0.0, 1.0);
     fragment_color = col * vec4(diffuseTint.rgb, 1.0);
 }
